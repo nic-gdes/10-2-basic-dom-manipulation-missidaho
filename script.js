41fa89ed3a7f0b23cfbae66fa1bfac7b4624b9a7
Madeleine <mgleahy@nic.edu>
2023-10-26 15:59:29 -0700

    title is complete

--- script.js
+++ script.js
@@ -3,9 +3,14 @@
 // 1. Link your javascript in the HTML above the closing </body> tag.
 
 // 2. Use querySelector to create a variable that contains the page's h1 title, and change the title using textContent into something a bit nicer.
+var title = document.getElementById("page-title");
+title.textContent = "Meet The Nice T. Rex!";
 
 // 3. Use querySelector to create a variable that contains the dinosaur image, and change the src to "./images/cute-dino.jpeg" so he doesn't look so mean. You can change an item's src by just calling the src method (item.src = "new_src";)
+var linkElement = document.querySelector('dino-img');
 
+
+console.log("Hello World");
 // 4. Use querySelector to create a variable for the document body, and change the background color to something less harsh and intense.
 
 // 5. Modify the following array to add at least 3 facts about the T. Rex that aren't so frightening. They don't need to be real facts.
